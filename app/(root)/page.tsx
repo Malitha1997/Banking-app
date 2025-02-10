de7712c84440d14230dfcfb4267d2c03f4a3978e
Malitha1997 <halahakoon10480@usci.ruh.ac.lk>
2025-02-10 21:19:41 +0530

    Implementing right side bar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
 import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar'
 import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
-  const loggedIn = { firstName: 'Malitha' }
+  const loggedIn = { firstName: 'Malitha' , lastName: 'Halahakoon', email:'hello@gmail.com' }
   return (
     <section className="home">
       <div className="home-content">
@@ -20,7 +21,13 @@ const Home = () => {
             totalCurrentBalance={1250.35}
           />
         </header>
+        RECENT TRANSACTIONS
       </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{},{}]}
+      />
     </section>
   )
 }
